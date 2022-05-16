6b6a80aa5dcd5e9ad6fe4dbec72a5994a703d94e
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-16 17:51:24 -0400

    completed first two exercises of the java console lesson

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,25 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        String myString = 3.14159;
+        String myString = ("La Bamba");
         System.out.println(myString);
-        float myNumber = 3.14;
+        float myNumber = 123L;
                 System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = ("hoochie mama");
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
     }
     int myFavoriteNumber = 14;
 
-
 }
+
+
