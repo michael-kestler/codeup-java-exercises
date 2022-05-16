f7c7aad9c23998737667989163375ebdc90fc303
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-16 15:32:43 -0400

    completed exercises 6, 7, and 8

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         String myString = 3.14159;
         System.out.println(myString);
-        long myNumber;
+        float myNumber = 3.14;
                 System.out.println(myNumber);
     }
     int myFavoriteNumber = 14;
