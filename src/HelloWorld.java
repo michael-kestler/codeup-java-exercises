1bbc7a6ae3c0b93ca4cf6fa4efe677728fd909e3
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-16 15:25:10 -0400

    changed value of myString to pi and got eerror

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        String myString = 5;
+        String myString = 3.14159;
         System.out.println(myString);
     }
     int myFavoriteNumber = 14;
