45ba544e059635b5870773ed0eea17f6a4d4fbae
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-16 15:23:03 -0400

    changed myString variable to a number as per 3rd exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        String myString = "La Bamba";
+        String myString = 5;
         System.out.println(myString);
     }
     int myFavoriteNumber = 14;
