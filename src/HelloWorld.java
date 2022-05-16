6df8f4e846d473d91e12267fbd21da618f8c57a8
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-16 15:21:22 -0400

    completed first two exercises on java synatx lesson

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        String myString = "La Bamba";
+        System.out.println(myString);
     }
+    int myFavoriteNumber = 14;
+
+
 }
