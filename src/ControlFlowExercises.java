012fef9f19c2fc68670510f19fd7b3e466b9bfeb
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-17 13:00:37 -0700

    completed fizzbuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -28,9 +28,24 @@ public class ControlFlowExercises {
 //        }
 
         //print squares of 2 less than 1,000,000
-        for(long i = 2; i<=1000000; i*=i){
-            System.out.println(i);
+//        for(long i = 2; i<=1000000; i*=i){
+//            System.out.println(i);
+//        }
+
+        //fizzbuzz exercise
+
+        for(int i = 1; i <= 100; i++){
+            if(((i % 3) == 0) && ((i % 5) == 0)) //Is it a multiple of 3 & 5
+                System.out.println("FizzBuzz");
+            else if ((i % 3) == 0) // is it a multiple of 3
+                System.out.println("Fizz");
+            else if ((i % 5) == 0) //is it a multiple of 5
+                System.out.println("Buzz");
+            else
+                System.out.println(i); //not a multiple of 3 or 5
+
         }
+
 
     }
 }
