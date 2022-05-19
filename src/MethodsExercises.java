b6d41a44fc820b0885b76ace66ba6c5f24a43990
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-19 08:27:02 -0700

    created methods to subtract and divide numbers and printed the results

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,14 +1,21 @@
 public class MethodsExercises {
     public static int addNumbers(int a, int b) {
-        int sum = a + b;
         // return value
-        return sum;
+        return a + b;
     }
 
     public static int multiplyNumbers(int a, int b){
-        int product = a* b;
-        return product;
+        return a * b;
     }
+
+    public static int subtractNumbers(int a, int b){
+        return a - b;
+    }
+
+    public static int divideNumbers(int a, int b){
+        return a / b;
+    }
+
 
 
     public static void main(String[] args){
@@ -21,6 +28,16 @@ public class MethodsExercises {
         int num4 = 5;
 
         System.out.println(multiplyNumbers(num3,num4));
+
+        int num5 = 100;
+        int num6 = 33;
+
+        System.out.println(subtractNumbers(num5, num6));
+
+        System.out.println(divideNumbers(num5, num4));
+
+
+
 
 
 
