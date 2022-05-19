951b351138a86b9bc0f4c61c47b32ccf132ce361
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-19 08:07:39 -0700

    created method to multiply two numbers and printed result

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -5,6 +5,11 @@ public class MethodsExercises {
         return sum;
     }
 
+    public static int multiplyNumbers(int a, int b){
+        int product = a* b;
+        return product;
+    }
+
 
     public static void main(String[] args){
         int num1 = 21;
@@ -12,7 +17,10 @@ public class MethodsExercises {
 
         System.out.println(addNumbers(num1, num2));
 
+        int num3 = 5;
+        int num4 = 5;
 
+        System.out.println(multiplyNumbers(num3,num4));
 
 
 
