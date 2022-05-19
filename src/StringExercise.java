2ed10168ee9c6b93776eee9b1560de64827643bb
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-19 13:26:05 -0700

    completed second string in string exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -7,5 +7,8 @@ public class StringExercise {
         System.out.println(message);
         message = message + " thought control";
         System.out.println(message);
+
+        String message2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.print(message2);
     }
 }
