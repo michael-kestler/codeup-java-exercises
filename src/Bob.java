6115744b63b18716e8c56ac975afdd7b0b79b24f
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-19 19:44:52 -0700

    added practice java practice exercises

--- src/Bob.java
+++ src/Bob.java
@@ -2,48 +2,21 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args){
-//        while (true) {
-            Scanner sc = new Scanner(System.in);
+
+            Scanner scanner = new Scanner(System.in);
             System.out.print("Say something to Bob: ");
-            String userString = sc.next();
+            String userString;
 
-
+            userString = scanner.nextLine();
             if (userString.endsWith("?")){
                 System.out.println("Sure.");
             } else if (userString.endsWith("!")){
                 System.out.println("Whoa, chill out!");
-            } else if (userString.equals("")){
+            } else if (userString.equals(" ")){
                 System.out.println("Fine. Be that way!");
             } else {
                 System.out.println("Whatever.");
             }
-
-//            switch ((userString.endsWith(userString + "?")) ? 0 :
-//                    (userString.endsWith(userString + "!")) ? 1 :
-//                    (userString.equals("")) ? 2 : 3){
-//                case 0:
-//                    System.out.println("Sure.");
-//                    break;
-//                case 1:
-//                    System.out.println("Whoa, chill out!");
-//                    break;
-//                case 2:
-//                    System.out.println("Fine. Be that way!");
-//                    break;
-//                case 3:
-//                    System.out.println("Whatever");
-//                    break;
-////                case 4:
-////                    System.out.println("You got an F loser");
-////                    break;
-////                case 5:
-////            }
-////            System.out.print("Continue? [y/N] ");
-////            String goAgain = sc.next();
-////            if (!goAgain.equals("y")) {
-////                break;
-////            }
-//        }
     }
 }
 
---
+++ src/Book.java
@@ -0,0 +1,7 @@
+public class Book {
+    public static void main (String[] args) {
+        Book artOfComputerPrograming = new Book();
+        Book effectiveJava = new Book();
+        Book cleanCode = new Book();
+    }
+}
