055ce0085e7c4cef50682c7fa23045efeed1f3d1
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-20 09:22:33 -0700

    made changes to input validation code

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,11 +1,24 @@
 package util;
 
 public class Input{
-
     private String scanner;
 
     public String getString(){
             return scanner;
     }
 
-}
+    public boolean yesNo(){
+        return scanner.equals("yes");
+
+    }
+
+//    public int getInt(int min, int max){
+//
+//
+//
+//        }
+    }
+
+
+
+
