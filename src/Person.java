e693c4449182367e88cf6f5b4ebc5a52f5185a9a
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-20 08:02:40 -0700

    completed first oop exercise

--- src/Person.java
+++ src/Person.java
@@ -3,15 +3,32 @@ public class Person {
 
     public String getName(){
 //TODO: return the person's name
-        return getName();
+        return name;
     }
 
     public void setName(String name){
 //TODO: change the name field to the passed value
+        this.name = name;
+
     }
+
+
     public void sayHello(){
 //TODO: print a message to the console using the person's name
+        System.out.println("Hello " + name);
     }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args){
+        Person person = new Person("John");
+        person.sayHello();
+    }
+
+
+
 
 
     }
