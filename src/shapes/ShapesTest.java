ce69958a95f9f5ad2bba4d464428048ba66e93e2
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-24 13:07:53 -0700

    added square class and constructor that accepts side as an argument. also created ShapesTest class to test the code

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public class ShapesTest {
+    public static void main (String [] args){
+
+    }
+
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Square extends Rectangle {
+        public Square (double side) {
+            set(side, side);
+        };
+
+}
+
+
+//create a class named Square inside of shapes package
+
+//define a constructor that accepts one argument side
+
+//Square should define a constructor that accepts one argument, side, and calls
+// the parent's constructor to
+// set both the length and width to the value of side.
