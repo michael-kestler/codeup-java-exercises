001f3aa450f9748f9bcecb2f2c42e59106d635c5
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-24 12:05:10 -0700

    created fields for width and length and methods for getting perimeter and area of rectangle

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,21 @@
 package shapes;
 
 public class Rectangle {
+    protected double width;
+    protected double length;
+
+    public void set(double l, double w){
+        length = l;
+        width = w;
+    }
+
+    public double getArea(){
+        return length * width;
+    }
+
+    public double getPerimeter(){
+        return 2 * (length + width);
+    }
 }
+
+
