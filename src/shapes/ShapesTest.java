78c6f2f73eb21a33a5f306778a9b7a586b300cd9
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-25 12:39:00 -0700

    created Measurable interface in the shapes directory

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,18 +2,18 @@ package shapes;
 
 public class ShapesTest {
     public static void main (String [] args){
-        Rectangle box1 = new Rectangle();
-        Rectangle box2 = new Square ();
-
-        box1.Rectangle(4, 5);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
-
-
-
-
-        System.out.println(box2.getArea());
-        System.out.println(box2.getPerimeter());
+//        Rectangle box1 = new Rectangle();
+//        Rectangle box2 = new Square ();
+//
+//        box1.Rectangle(4, 5);
+//        System.out.println(box1.getPerimeter());
+//        System.out.println(box1.getArea());
+//
+//
+//
+//
+//        System.out.println(box2.getArea());
+//        System.out.println(box2.getPerimeter());
 
 
 
