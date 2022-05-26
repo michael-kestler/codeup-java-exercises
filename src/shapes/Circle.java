84d2659ff0afda6087e26bac37baa1a1bf5aa0ed
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-26 13:25:14 -0700

    created grades directory and Student class for collections exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,11 +1,31 @@
 package shapes;
 
 public class Circle {
+    private double radius;
+    public static int count = 0;
+    // THE FINAL KEYWORD
+    final public static double PI = Math.PI;
 
-private class radius{
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
+    public double getArea() {
+        return Math.PI * (Math.pow(radius, 2));
+    }
+
+    public double getCircumference() {
+        return 2* Math.PI * radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
 
 
-}
 
