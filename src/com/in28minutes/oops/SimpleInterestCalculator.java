772a77cea504b29b3fd26f743e9e468af3441c03
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-28 11:42:21 -0700

    created simple interest calculator in java exercises

--- src/com/in28minutes/oops/SimpleInterestCalculator.java
+++ src/com/in28minutes/oops/SimpleInterestCalculator.java
@@ -1,6 +1,20 @@
 package com.in28minutes.oops;
 
+import java.math.BigDecimal;
+
 public class SimpleInterestCalculator {
 
+    private BigDecimal principal;
+    private BigDecimal interest;
 
+    public SimpleInterestCalculator(String principal, String interest) {
+        this.principal = new BigDecimal(principal);
+        this.interest = new BigDecimal(interest);
+    }
+
+    public BigDecimal calculateTotalValue(int years){
+        BigDecimal totalValue =
+                principal.add(principal).add(principal.multiply(interest).multiply(BigDecimal.valueOf(years)));
+        return totalValue;
+    }
 }
