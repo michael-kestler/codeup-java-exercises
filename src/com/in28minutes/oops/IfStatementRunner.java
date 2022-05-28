77b139f581a2f5d964f25a6715a40372f3feab61
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-28 15:19:24 -0700

    created menu runner exercise

---
+++ src/com/in28minutes/oops/IfStatementRunner.java
@@ -0,0 +1,23 @@
+package com.in28minutes.oops;
+
+public class IfStatementRunner {
+    public static void main(String[] args) {
+//        int i = 26;
+//        //i is 25
+//        //i is 24
+//        //i is neither 25 or 24
+//        if(i==25) {
+//            System.out.println("i=25");
+//        }else if (i==24){
+//            System.out.println("i =24");
+//        } else {
+//            System.out.println("i != 24 and i !=25");
+//        }
+//        int i = 0;
+//
+//        if(i==1) {
+//            System.out.println("i");        }
+
+
+    }
+}
