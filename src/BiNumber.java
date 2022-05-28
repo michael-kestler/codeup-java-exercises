fdb648d7044479b7f27923d287f4697524469570
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-27 17:22:26 -0700

    created class to practice java exercises

---
+++ src/BiNumber.java
@@ -0,0 +1,23 @@
+public class BiNumber {
+
+        public int a;
+        public int b;
+
+    BiNumber(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+        BiNumber numbers = new BiNumber(2, 3);
+
+
+
+
+
+    }
+}
--- src/PrimitiveNotes.java
+++ src/PrimitiveNotes.java
@@ -1,8 +1,22 @@
 public class PrimitiveNotes {
     public static void main(String[] args) {
+
         System.out.println(Byte.SIZE);
         System.out.println(Integer.MAX_VALUE);
         System.out.println(Short.MAX_VALUE);
+
+//        int i = 10;
+//
+//        int j = --i;
+//
+//
+//
+//        System.out.println(j);
+
+
+
+
+
     }
 
 }
