46baa7f6fe3159c16bee20ce0bcd23345329d190
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-31 07:55:58 -0700

    added try catch block to exception exercise

--- src/com/in28minutes/oops/ExceptionHandlingRunner2.java
+++ src/com/in28minutes/oops/ExceptionHandlingRunner2.java
@@ -2,6 +2,25 @@ package com.in28minutes.oops;
 
 public class ExceptionHandlingRunner2 {
     public static void main(String[] args) {
+        method1();
+        System.out.println("Main ended");
+    }
+
+    private static void method1() {
+        method2();
+        System.out.println("Method1 Ended");
+    }
+
+
+    private static void method2() {
+        try {
+            String str = null;
+            str.length();
+            System.out.println("Method2 Ended");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
+
