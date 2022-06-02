a4f43b2aa13105c800845490e6493024d4926c68
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-01 18:38:44 -0700

    created ascii table to be displaye in the console

--- src/Contacts/ContactsManagerRunner.java
+++ src/Contacts/ContactsManagerRunner.java
@@ -14,7 +14,7 @@ import static java.nio.file.StandardOpenOption.APPEND;
 public class ContactsManagerRunner {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        Path pathFileToRead = Paths.get("/Users/michaelkestler/IdeaProjects/codeup-java-exercises/src/Contacts/contacts.txt");
+        Path pathFileToRead = Paths.get("src", "Contacts", "contacts.txt");
         Path pathFileToWrite = Paths.get("/Users/michaelkestler/IdeaProjects/codeup-java-exercises/src/Contacts/contacts.txt");
 
         //            List<String> lines = Files.readAllLines(pathFileToRead);
@@ -32,10 +32,20 @@ public class ContactsManagerRunner {
         System.out.println("5. Exit.");
         System.out.println("(Enter an option 1, 2, 3, 4, or 5): ");
         int chooseOperation = scanner.nextInt();
-
+//        display contact
         if (chooseOperation == 1) {
             Files.lines(pathFileToRead)
                     .forEach(System.out::println);
+            String leftAlignFormat = "| %-15s | %-15d |%n";
+
+            System.out.format("+-----------------+-----------------+%n");
+            System.out.format("| Name            | Phone Number    |%n");
+            System.out.format("+-----------------+-----------------+%n");
+            Files.lines(pathFileToRead)
+                    .forEach(System.out::println);
+            System.out.format("+-----------------+------------------+%n");
+
+            //user can add a contact
         } else if (chooseOperation == 2) {
             Scanner scanner1 = new Scanner(System.in);
             System.out.println("Enter name and phone number:");
