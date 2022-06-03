2bb9ac9a78420ef0cb7f936bf3880c438d923666
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-02 20:56:04 -0700

    completed review exercise number 4. created carShop package and a new class called Car. created private instance fields for make, model, color, and year. created constructor that sets those properties, and created gettters and setters for each of the properties

---
+++ src/carShop/Car.java
@@ -0,0 +1,57 @@
+package carShop;
+
+public class Car {
+    //fields that cannot be accessed outside of Car
+    private String model;
+    private String make;
+    private String color;
+    private int year;
+
+    //constructor that sets model, make, color, and year
+    public Car(String model, String make, String color, int year) {
+        this.model = model;
+        this.make = make;
+        this.color = color;
+        this.year = year;
+    }
+
+    //write getters and setters for each property
+        public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+
+
+
+
+
+}
---
+++ src/carShop/CarRunner.java
@@ -0,0 +1,7 @@
+package carShop;
+
+public class CarRunner {
+    public static void main(String[] args) {
+        Car car = new Car("Accord", "Honda", "Black", 1999);
+    }
+}
