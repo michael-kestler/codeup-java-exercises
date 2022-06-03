d6c54858d3c59f6d4e666dfceaf7d80b0f89f5d2
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-02 21:04:02 -0700

    created dealership app class that contains main method for running car and customer class methods

--- src/carShop/DealershipApp.java
+++ src/carShop/DealershipApp.java
@@ -1,6 +1,6 @@
 package carShop;
 
-public class CarRunner {
+public class DealershipApp {
     public static void main(String[] args) {
         Car car = new Car("Accord", "Honda", "Black", 1999);
     }
