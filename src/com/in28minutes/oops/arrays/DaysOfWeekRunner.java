8def7b6491f19ce820edae1dd1e04f92af91fc1b
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-04 19:39:33 -0700

    refactored method that gets longest string in array. created method that reversed order of array when printed

--- src/com/in28minutes/oops/arrays/DaysOfWeekRunner.java
+++ src/com/in28minutes/oops/arrays/DaysOfWeekRunner.java
@@ -6,30 +6,53 @@ import java.util.Collections;
 public class DaysOfWeekRunner {
 
     public static void main(String[] args) {
-        String daysList[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        String[] daysList = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-        System.out.println(getLongestString(daysList));
+//        System.out.println(getLongestString(daysList));
 
 //        System.out.println(reverseArr(daysList));
-    }
 
+        String dayWithMostCharacters = "";
+        for (String day : daysList) {
+            if (day.length() > dayWithMostCharacters.length()) {
+                dayWithMostCharacters = day;
+            }
+        }
+        System.out.println("Day with Most number of characters " + dayWithMostCharacters);
 
-    public static String getLongestString(String[] arr) {
-        int index = 0;
-        int elementLength = arr[0].length();
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i].length() > elementLength) {
-                index = i;
-                elementLength = arr[i].length();
-
+        for (int i = 0; i < daysList.length; i++) {
+            System.out.println(daysList[i]);
+            System.out.println();
+        }
+            for (int i = daysList.length-1; i >= 0; i--) {
+                System.out.println(daysList[i]);
+                System.out.println();
 
             }
-
-
         }
-        return arr[index];
+
+
     }
+
+
+
+
+//    public static String getLongestString(String[] arr) {
+//        int index = 0;
+//        int elementLength = arr[0].length();
+//
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i].length() > elementLength) {
+//                index = i;
+//                elementLength = arr[i].length();
+//
+//
+//            }
+
+
+//        }
+//        return arr[index];
+
 
 //    public static void String reverseArr(String[] arr){
 //
@@ -38,4 +61,4 @@ public class DaysOfWeekRunner {
 //        return arr[];
 //    }
 
-    }
+
