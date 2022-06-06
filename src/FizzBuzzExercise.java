1b5de5f3c3204006c28f0803a0740be8f3f4e706
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-06 08:10:46 -0700

    completed fizz buzz warmup exercise

---
+++ src/FizzBuzzExercise.java
@@ -0,0 +1,20 @@
+public class FizzBuzzExercise {
+
+    public static void main(String[] args) {
+
+
+
+        for (int i = 1; i <= 100; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) // Is it a multiple of 3 & 5?
+                System.out.println("fizzbuzz");
+            else if ((i % 3) == 0) // Is it a multiple of 3?
+                System.out.println("fizz");
+            else if ((i % 5) == 0) // Is it a multiple of 5?
+                System.out.println("buzz");
+            else
+                System.out.println(i); // Not a multiple of 3 or 5
+        }
+
+
+    }
+}
