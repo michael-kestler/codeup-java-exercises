a5dc719f483ec1643e5757286ada3d9468089f0c
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-12 17:31:39 -0700

    completed server name generator task of arrays exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -3,18 +3,25 @@ import java.util.Random;
 
 public class ServerNameGenerator {
 
-    public static void main(String[] args) {
+    public static String[] adjectives = {"funny", "serious", "cute", "handsome", "beautiful", "mysterious", "fortunate",
+            "responsible", "prosperous"};
 
-        String[] adjectives = {"funny", "serious", "cute", "handsome", "beautiful", "mysterious", "fortunate",
-                "responsible", "prosperous"};
+    public static String[] nouns = {"earth", "sun", "moon", "water", "wind", "fire", "platinum", "gold", "copper",
+            "silver"};
 
-        String[] nouns = {"earth", "sun", "moon", "water", "wind", "fire", "platinum", "gold", "copper", "silver"};
-
-
+    public static String randomName() {
         String randomAdj = (adjectives[new Random().nextInt(adjectives.length)]);
         String randomNoun = (nouns[new Random().nextInt(nouns.length)]);
+        return (randomAdj + "-" + randomNoun);
 
-        System.out.println("Here is your server name: " + randomAdj + "-" + randomNoun);
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Here is your server name: ");
+        System.out.println(randomName());
 
 
     }
