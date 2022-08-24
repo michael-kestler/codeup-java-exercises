eb9a696ddbcf2e4ce11b8fa6a6aa357634c0be94
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-08-23 21:24:28 -0700

    added comments to non-matching characters method

--- src/PracticeExercises/NonMatchingCharacters.java
+++ src/PracticeExercises/NonMatchingCharacters.java
@@ -14,7 +14,15 @@ public class NonMatchingCharacters {
         System.out.println("Enter a word: ");
         String text = scanner.nextLine();
 
+        //Hash Map data structure is taken which works with the key
+        //value pair
+
         HashMap<Character, Integer> mp = new HashMap<>();
+
+        //loop the string character by character, and verify if that character exists in the
+        //hash map or not
+        //if the result is true the counter for the character in the hashmap is increased or
+        //else put a count of 1
         for(int i = 0; i<text.length(); i++) {
             char ch = text.charAt(i);
             if (mp.containsKey(ch)) {
@@ -24,6 +32,10 @@ public class NonMatchingCharacters {
                 mp.put(ch, 1);
             }
         }
+
+        //once loop ends, then Hash Map is traversed and print the characters with count equal to 1
+        //example when 'remember' is input 'b-1' is what is printed
+
             Set<Character> charct = mp.keySet();
             for (Character ch: charct){
                 int c= mp.get(ch);
@@ -31,10 +43,6 @@ public class NonMatchingCharacters {
                     System.out.println(ch + " - " + c);
                 }
             }
-
-
-
-
 
     }
 
